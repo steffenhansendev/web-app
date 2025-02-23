24402beaa38346bc7dbd73c71623e1910e52ee8b
Steffen Hansen <mail@steffen-hansen.dk>
2025-02-23 13:16:29 +0100

    Scaffold minimal dependencies and structure

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React, {JSX} from "react";
+
+function App(): JSX.Element {
+    return (<p>Web App</p>);
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import ReactDOM, {Root} from "react-dom/client";
+import React from "react";
+import App from "./App";
+
+const root: Root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+    <App/>
+);
