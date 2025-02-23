18efd1546dbc812e6a1d55c9c4df949b70910f3b
Steffen Hansen <mail@steffen-hansen.dk>
2025-02-23 13:16:29 +0100

    Scaffold minimal dependencies and structure

---
+++ README.md
@@ -0,0 +1,12 @@
+# Web App Template
+## Technology
+- Node Package Manager
+- TypeScript
+- React
+- React DOM
+- Babel
+- Webpack
+
+`npm run develop` serves the application with webpack-dev-server in watch mode
+
+`npm run pack` prepares in /dist the application for production deployment
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js']
+    }, module: {
+        rules: [{
+            test: /\.(tsx|ts)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader', options: {
+                    presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react']
+                }
+            }
+        }, {
+            test: /\.(jsx|js)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader', options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+        }]
+    }, output: {
+        clean: true
+    }, plugins: [new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'public', 'index.html')
+    })]
+}
+
