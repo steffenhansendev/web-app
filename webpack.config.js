a2e40a1e9457b82ac5505fdf4138b92169f90a96
Steffen Hansen <mail@steffen-hansen.dk>
2025-03-27 18:18:12 +0100

    Provide environment determination in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,40 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const PRODUCTION = "production";
 
-module.exports = {
-    resolve: {
-        extensions: ['.tsx', '.ts', '.jsx', '.js']
-    }, module: {
-        rules: [{
-            test: /\.(tsx|ts)$/, exclude: /node_modules/, use: {
-                loader: 'babel-loader', options: {
-                    presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react']
-                }
-            }
-        }, {
-            test: /\.(jsx|js)$/, exclude: /node_modules/, use: {
-                loader: 'babel-loader', options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react']
-                }
-            }
-        }, {
-            test: /\.css$/i, exclude: /\.module\.css$/, use: ['style-loader', 'css-loader']
-        }, {
-            test: /\.module\.css$/i, use: ['style-loader', {
-                loader: 'css-loader', options: {
-                    modules: {
-                        namedExport: false
+module.exports = (env, argv) => {
+    const isProductionPack = argv.mode === PRODUCTION;
+    return {
+        resolve: {
+            extensions: ['.tsx', '.ts', '.jsx', '.js']
+        }, module: {
+            rules: [{
+                test: /\.(tsx|ts)$/, exclude: /node_modules/, use: {
+                    loader: 'babel-loader', options: {
+                        presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react']
                     }
                 }
-            }]
-        },]
-    }, output: {
-        clean: true
-    }, plugins: [new HtmlWebpackPlugin({
-        template: path.join(__dirname, 'public', 'index.html')
-    })]
+            }, {
+                test: /\.(jsx|js)$/, exclude: /node_modules/, use: {
+                    loader: 'babel-loader', options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }, {
+                test: /\.css$/i, exclude: /\.module\.css$/, use: ['style-loader', 'css-loader']
+            }, {
+                test: /\.module\.css$/i, use: ['style-loader', {
+                    loader: 'css-loader', options: {
+                        modules: {
+                            namedExport: false
+                        }
+                    }
+                }]
+            },]
+        }, output: {
+            clean: true
+        }, plugins: [new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html')
+        })]
+    }
 }
-
